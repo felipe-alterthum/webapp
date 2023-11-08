5fcbc1290e1700a8ad7c5129ba8997417594156f
felipe_alterthum <fethumf@gmail.com>
2023-11-08 10:11:36 -0300

    add segundo botao

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # test_vscode
 
-Esse projeto tem como objetivo a criação de um app web com um headline e um botão.
+Esse projeto tem como objetivo a criação de um app web com um headline e dois botões.
 
-O botão produzirá um histograma baseado em um conjunto de dados.
+O primeiro botão produzirá um histograma baseado em um conjunto de dados de veículos dos Estados Unidos.
+
+O segundo botaõ criará um gráfico de dispersão de Km vs preço da mesma base de dados.
--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import pandas as pd
 import plotly_express as px
 
 st.header('Esse é o meu primeiro aplicativo web', divider ='blue')
-st.header('O app foi feito utilizando red[*Streamlit*]')
+st.header('O app foi feito utilizando :red[**Streamlit**] 	:star2:')
 
-car_data = pd.read_csv('/notebooks/vehicles.csv') # lendo os dados
+car_data = pd.read_csv('C:/Users/Felipe/Projetos_pessoais/test_vscode/notebooks/vehicles.csv') # lendo os dados
 hist_button = st.button('Criar histograma')
 
 if hist_button:
@@ -16,7 +16,16 @@ if hist_button:
     fig = px.histogram(car_data, x="odometer")
 
     #Exibindo
-    st.plotly_chart(fig, use_container_width=True) 
+    st.plotly_chart(fig, use_container_width=True)
 
+disp_button = st.button('Criar gráfico de dispersão')
 
+if disp_button:
+    #Escrevendo a mensagem
+    st.write('Gráfico de dispersão kilometragem vs preço')
 
+    #Criando a dispersão
+    fig = px.scatter(car_data, x="odometer", y="price") 
+    
+    #Exibindo
+    st.plotly_chart(fig, use_container_width=True)
